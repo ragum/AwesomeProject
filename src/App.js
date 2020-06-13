58d5e5e7c9d3724c5c48fde10f00a0a446273703
ragum <ragum@icube.us>
2020-06-13 09:29:03 +0700

    todo list

--- src/App.js
+++ src/App.js
@@ -6,46 +6,42 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  View,
   Text,
-  TextInput,
   SafeAreaView,
-  Image,
-  Alert,
+  Platform,
   TouchableOpacity,
+  Alert,
+  View,
+  Image,
 } from 'react-native';
 import styles from './asset/style';
+import ActivityForm from './component/TodoForm';
+import ActivityList from './component/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const dataActivityParent = (data) => {
+    setTodos(todos.concat(data));
+  };
   return (
     <SafeAreaView>
-      <View>
-        <View style={styles.header}>
-          <Image
-            style={styles.image}
-            source={{
-              uri: 'https://reactnative.dev/img/tiny_logo.png',
-            }}
-          />
-          <Text style={styles.title}>Login</Text>
-        </View>
-        <View style={styles.box}>
-          <Text style={styles.label}>Username</Text>
-          <TextInput placeholder="Username" style={styles.inputText} />
-          <Text style={styles.label}>Password</Text>
-          <TextInput
-            secureTextEntry={true}
-            placeholder="Password"
-            style={styles.inputText}
-          />
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => Alert.alert('monggo')}>
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableOpacity>
-        </View>
+      <View style={styles.header}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        <Text style={styles.title}>PEER</Text>
+      </View>
+      <View style={styles.box}>
+        <ActivityForm dataActivity={dataActivityParent} />
+      </View>
+
+      <View style={styles.box}>
+        <ActivityList ListTodo={todos} />
       </View>
     </SafeAreaView>
   );
