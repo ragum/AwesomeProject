e013a66e102651ab0d7b2c8fd12b0a77f4be2978
ragum <ragum@icube.us>
2020-06-27 10:47:58 +0700

    final assesment

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,12 +14,14 @@ import {
   SafeAreaView,
   Text,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import Category from '../Category';
 import styles from '../../asset/todo/style';
+import {connect} from 'react-redux';
 
-const Home = ({route, navigation}) => {
-const { email } = route.params;
+const Home = ({route, navigation, auth}) => {
   const gotoLanding = () => {
     navigation.navigate('Landing');
   };
@@ -32,21 +34,74 @@ const { email } = route.params;
     gotoLanding();
     console.log('Done.');
   };
+  function Item({title}) {
+    // const {user} = auth;
+    // console.log(user);
+    return (
+      <View>
+        <Text>{title}</Text>
+      </View>
+    );
+  }
+  const notification = [
+    {
+      __typename: 'DataNotification',
+      content: 'template is not found',
+      createdAt: '2020-06-13 05:03:04',
+      entityId: '0',
+      subject: 'Welcome to lotte mart Indonesia',
+      unread: 'false',
+    },
+    {
+      __typename: 'DataNotification',
+      content: 'template is not found',
+      createdAt: '2020-06-13 05:03:04',
+      entityId: '1',
+      subject: 'Welcome to lotte mart Indonesia',
+      unread: 'false',
+    },
+    {
+      __typename: 'DataNotification',
+      content: 'template is not found',
+      createdAt: '2020-06-13 05:03:04',
+      entityId: '2',
+      subject: 'Welcome to lotte mart Indonesia',
+      unread: 'false',
+    },
+    {
+      __typename: 'DataNotification',
+      content: 'template is not found',
+      createdAt: '2020-06-13 05:03:04',
+      entityId: '3',
+      subject: 'Welcome to lotte mart Indonesia',
+      unread: 'false',
+    },
+  ];
+  const listNotification = () => {
+    return (
+      <FlatList
+        data={notification}
+        renderItem={({item}) => <Item title={item.content} />}
+        keyExtractor={(item) => item.entityId}
+      />
+    );
+  };
   return (
     <>
       <SafeAreaView>
-        <ScrollView>
           <View>
             <Text style={styles.h1}>Sukses Login!!</Text>
-            <Text style={styles.welcome}>Welcome {email}</Text>
           </View>
-		  <TouchableOpacity style={styles.buttonLogout} onPress={logOut}>
-              <Text style={styles.buttonText}>Log Out</Text>
-            </TouchableOpacity>
-        </ScrollView>
+          <Category />
+          <TouchableOpacity style={styles.buttonLogout} onPress={logOut}>
+            <Text style={styles.buttonText}>Log Out</Text>
+          </TouchableOpacity>
       </SafeAreaView>
     </>
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps, null)(Home);
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -8,17 +8,24 @@
  */
 
 import React from 'react';
-import {View, Button} from 'react-native';
+import {View, Button, TouchableOpacity, Text} from 'react-native';
+import sty from '../../asset/assesment/final';
 
 const Landing  = ({navigation}) => {
   return (
     <View>
-      <Button
+      	<Button
           title="Login"
           onPress={() =>
             navigation.navigate('Login')
           }
         />
+		<TouchableOpacity style={sty.button} onPress={() => navigation.navigate('Plp')}>
+            <Text style={sty.buttonText}>PLP</Text>
+		</TouchableOpacity>
+		<TouchableOpacity style={sty.button} onPress={() => navigation.navigate('Pdp')}>
+            <Text style={sty.buttonText}>PDP</Text>
+		</TouchableOpacity>
   </View>
   );
 };
